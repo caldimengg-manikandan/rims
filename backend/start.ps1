0915fd441bf423f9364baa04091ea2fff0e993b0
MPradeep-08 <pradeepmuthuselvan08@gmail.com>
2026-06-24 16:21:43 +0530

    trigger redeploy to vps

--- backend/start.ps1
+++ backend/start.ps1
@@ -3,7 +3,7 @@ Set-Location $PSScriptRoot
 $PORT = 10000
 $PID_FILE = ".\.backend.pid"
 
-# trigger redeploy 17 06 26: 18 49
+# trigger redeploy 2026-06-24: 16:21
 
 # ---------------------------------------------------------------------------
 # Aggressively free port 10000 regardless of whether the process is alive.
